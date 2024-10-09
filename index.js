8bc2f5a0f5eb9953e4c01b97d6c75a05142e064d
Jason Ziegler <jason@insightfulautomation.com>
2024-10-09 06:26:46 -0500

    display todo item

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 // index.js
 console.log('hello world');
+
+const todoUl = document.getElementById("thingsToDoList");
+
+// console.log(document.getElementsByTagName('input')[0]);
+
+// create an empty list to hold todos
+const thingsToDoList = [];
+// create an empty list to hold completed items
+const completedToDoList = [];
+// add an event listener to the "addTodoButton" button
+// create an onClick function
+function addSomethingToDo(thingToDo) {
+  // // capture input
+// // push to todoList
+thingsToDoList.push(thingToDo);
+// // clear "addTodoButton" input
+}
+
+// add todos to the DOM
+function displayThingsToDo(thingsToDo) {
+  console.log("thingsToDo: ", thingsToDo);
+}
+
+
+let listItem = document.createElement('li');
+listItem.textContent = "Display Text Content";
+
+todoUl.append(listItem);
