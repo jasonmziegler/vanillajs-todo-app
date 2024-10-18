e3ba866ee225e479d0b321a16ffe40904ce88943
Jason Ziegler <jason@insightfulautomation.com>
2024-10-18 06:56:30 -0500

    update editToDo and add handleUpdateClick

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const addToDoButton =document.getElementById("addToDoButton");
 
 addToDoInput.value = "";
 
-console.log(addToDoInput);
+// console.log(addToDoInput);
 // create an empty list to hold todos
 const thingsToDoList = [];
 // create an empty list to hold completed items
@@ -23,10 +23,6 @@ thingsToDoList.push(thingToDo);
 displayThingToDo(thingsToDoList);
 } // end of addSomethingToDo
 
-// create handle on click edit button function
-
-// create handle click delete button function
-
 function captureTodoInput(){
   // read what is in the input and store in a variable
   let input = addToDoInput.value;
@@ -38,7 +34,6 @@ function captureTodoInput(){
   return input;
 }
 
-
 function addToDoButtonOnClick() {
 // console.log("hello");
 addSomethingToDo(captureTodoInput());
@@ -46,10 +41,36 @@ addSomethingToDo(captureTodoInput());
 
 addToDoButton.onclick = addToDoButtonOnClick;
 
-function editToDo(index) {
-console.log("Edit Button Clicked: ", thingsToDoList[index]);
+// create handle on click edit button function
+function editToDo(event, index) {
+  console.log("Edit Button Clicked: ", thingsToDoList[index]);
+  console.log("Event: ", event);
+  let textNode = event.target.previousSibling;
+  let editButton = event.target;
+  let parentNode = event.target.parentNode;
+  console.log("Todo Text", textNode);
+  let todoInput = document.createElement("input");
+  todoInput.type = "text";
+  todoInput.value = textNode.data;
+  parentNode.removeChild(textNode);
+  // remove the edit button
+  parentNode.removeChild(editButton);
+  // create a button that will confirm the edit // send event, index
+  let updateButton = document.createElement("button");
+  updateButton.textContent = "Update";
+  updateButton.onclick = (event) => handleClickUpdate(event, index);
+  parentNode.insertBefore(updateButton, parentNode.firstChild);
+  parentNode.insertBefore(todoInput, parentNode.firstChild);
+  
+
+}
+// create a handle click update button function 
+function handleClickUpdate(event, index) {
+  console.log("Event: ", event);
+  console.log("Index: ", index);
 }
 
+// create handle click delete button function
 function deleteToDo(index) {
   console.log("deleted button clicked: ", thingsToDoList[index]);
   let itemRemoved = thingsToDoList.splice(index, 1);
@@ -105,21 +126,20 @@ function displayThingToDo(updatedToDoList) {
     // add edit button
     button = document.createElement('button');
     button.textContent = "edit";
-    button.onclick = () => editToDo(index);
+    // add event listener and
+    button.onclick = (event) => editToDo(event,index);
     listItem.appendChild(button);
+    // add delete button
     button = document.createElement('button');
     button.textContent = "delete";
     button.onclick = () => deleteToDo(index);
     listItem.appendChild(button);
     button = document.createElement('button');
+     // add complete button
     button.textContent = "complete";
     button.onclick = () => completeToDo(index);
     listItem.appendChild(button);
-// add event listener and
-// add delete button
-// add event listener
-// add to list item
-    
+    // add to list item
     todoUl.append(listItem);
   });
 
